fc6eeee3147e1005af247c4b301dcfd499dc6240
rasputin <rasputin@darkares.dk>
2019-06-20 18:26:50 +0200

    Added extra information

--- HolteAkupunktur/ClientApp/src/components/Home/Clinic.js
+++ HolteAkupunktur/ClientApp/src/components/Home/Clinic.js
@@ -14,6 +14,8 @@ export class Clinic extends Component {
                     <p>Bing er uddannet på Institution of Acupuncture and Moxibustion China Academy of Chinese medical
 Sciences i Beijing.</p>
                     <p>I år 2000 blev jeg uddannet på University of traditionel Chinese medicin i LiaoNing </p>
+                    <p>Denne skole er verdens mest anerkendte akupunktur skole og folk fra hele verden kommer hertil for at lære akupunkturens kunst.
+                        Bing har også uddannet sig i Danmark og er godkendt af RAB akupunktur union. Du kan få en professionel kinesisk akupunktur behandling.</p>
 <p>Jeg har arbejdet hos forskellige Københavnske klinikker igennem nogle år og har således opnået stor erfaring og ekspertise.
 Jeg lægger vægt på, at behandlingerne sker i en atmosfære præget af stor tryghed.</p>
                 </Col>
